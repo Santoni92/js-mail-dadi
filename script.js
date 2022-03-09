d796968c3f42f7b1641e88949375820ddd87c6d1
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-09 15:20:39 +0100

    scitto lo pseudocodice;come tratto il dato che rappresenta l'email?

---
+++ script.js
@@ -0,0 +1,15 @@
+/****************************MAIL***************************/
+//PSEUDOCODICE
+//1. crea array con dentro le email 
+//2. inizializza n° massimo N di tentativi di inserimento email da parte dell'utente
+//3. chiedi email all'utente
+//4. PER i da 1 N ESEGUI
+//4a.      SE email è dentro l'array 
+//         ALLORA stampa messaggio 
+//         ALTRIMENTI chiedi nuovamente l'email
+//         FINE SE
+//  RIPETI
+
+
+console.log('JS OK');   //per controllare che lo script js sia linkato dal codice html
+
