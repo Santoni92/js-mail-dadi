871f5c2025c4340a51377774859f2c993b72abfb
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-09 17:05:15 +0100

    aggiunta pseudocodice  ed implementazione in js del secondo esercizio

--- script.js
+++ script.js
@@ -15,10 +15,9 @@ console.log('JS OK');   //per controllare che lo script js sia linkato dal codic
 
 const numeroMassimoInserimenti = Math.floor((Math.random() * 10) + 1);  //il numero massimo di tentativi di inserimento dell'email da parte dell'utente va da 1 a 10
 
-let arrayEmail = ["silvio@gmail.com","lorenzo@gmail.com","federico@gmail.com"];
+let arrayEmail = ["silvio.antonioli@gmail.com","lorenzo.rossi@gmail.com","federico.bianchi@gmail.com"];
 
 let emailUtente = prompt('Inserisci la tua email:');
-
 for(let indice = 0; indice < numeroMassimoInserimenti; indice++)
 {
     for(let i = 0; i < arrayEmail.length; i++)
@@ -34,3 +33,29 @@ for(let indice = 0; indice < numeroMassimoInserimenti; indice++)
     }
 }
 alert('Hai superato il numero massimo di tentativi, accesso negato!!!');
+
+/**************************GIOCO DADI***********************************/
+//PSEUDOCODICE
+//1. genero numero per il giocatore
+//2 genero numero per il computer
+//3. SE n° giocatore = n° computer
+//   ALLORA giocatore e computer pareggiano
+//   ALTRIMENTI
+//              SE n° giocatore > n° computer
+//              ALLORA vince giocatore
+//              ALTRIMENTI vince computer
+
+console.log('JS OK');   //per controllare che lo script js sia linkato dal codice html
+
+const numeroGiocatore = Math.floor((Math.random() * 5) + 1);
+const numeroComputer = Math.floor((Math.random() * 5) + 1);
+
+if(numeroGiocatore === numeroComputer)
+{
+    console.log('pareggio!!!');
+}else if(numeroGiocatore > numeroComputer)
+{
+    console.log('Vince il giocatore!!!');
+}else{
+    console.log('Vince il computer!!!');
+}
